c4b76e31eb72bd54b0968c85d8f46c7a8f2c74d5
stayFoolish <nemanjakaraklajic90@icloud.com>
2023-12-21 00:23:20 +0100

    frontend-starter: starter backend logic

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import products from "./data/products.js";
+import dotenv from "dotenv";
+dotenv.config();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
+app.get("/api/products", (req, res) => {
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const response = products.find((product) => product._id === req.params.id);
+
+  res.json(response);
+});
+
+app.listen(port, () => console.log(`Server running on: ${port}`));
--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -1,8 +1,37 @@
 import { Row, Col } from "react-bootstrap";
-import products from "../../products";
+// import products from "../../products";
 import Product from "../../components/Product";
+import { useEffect, useState } from "react";
+import { type IProduct } from "../../types";
+import { getProductsAPI } from "../../apis/getProducts";
 
 const Home = () => {
+  const [products, setProducts] = useState<IProduct[]>([]);
+
+  console.log("products: ", products);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const fetchedProducts = await getProductsAPI();
+
+        console.log(fetchedProducts);
+
+        if (fetchedProducts && !(fetchedProducts instanceof Error)) {
+          setProducts(fetchedProducts);
+        } else {
+          console.error("Error while fetching products");
+        }
+
+        console.log("fetchedProducts: ", fetchedProducts);
+      } catch (error) {
+        if (error instanceof Error) console.log(error.message);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
     <>
       <h1>Latest Products</h1>
