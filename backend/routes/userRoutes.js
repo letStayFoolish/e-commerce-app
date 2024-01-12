74499d4e021a3b19f194219a26f43e7e5b484f1f
stayFoolish <nemanjakaraklajic90@icloud.com>
2024-01-12 17:47:11 +0100

    backend-auth: Auth Middleware & Endpoint

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -10,13 +10,22 @@ import {
   deleteUser,
   updateUser,
 } from "../controllers/userController.js";
+import { protect, admin } from "../middleware/authMiddleware.js";
+
 const router = express.Router();
 
 // because of we use mongoose, we are working with async code so we need this middleware asyncHandler
-router.route("/").post(registerUser).get(getUsers);
+router.route("/").post(registerUser).get(protect, admin, getUsers);
 router.post("/logout", logoutUser);
 router.post("/login", authUser);
-router.route("/profile").get(getUserProfile).put(updateUserProfile);
-router.route("/:id").delete(deleteUser).get(getUserByID).put(updateUser);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
+router
+  .route("/:id")
+  .delete(protect, admin, deleteUser)
+  .get(protect, admin, getUserByID)
+  .put(protect, admin, updateUser);
 
 export default router;
