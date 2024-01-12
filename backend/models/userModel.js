29167ef18cb5a42716ac46747d586a3304d7a240
stayFoolish <nemanjakaraklajic90@icloud.com>
2024-01-12 19:27:08 +0100

    backend-auth: User Register Endpoint & Encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,16 @@ userSchema.methods.matchPasswords = async function (enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+
+  const salt = await bcrypt.genSalt(10);
+
+  this.password = await bcrypt.hash(this.password, salt);
+}); // .pre --> before saving to the database
+
 const User = mongoose.model("User", userSchema);
 
 export default User;
